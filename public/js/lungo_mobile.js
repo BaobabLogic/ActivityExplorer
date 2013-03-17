1add698b7001183dec6124a324268a6e5d36abb4
Courtney Jooste <darklordvivimord@github.com>
2013-03-17 22:27:23 +0200

    lungo working in the partialgit add . :D

--- public/js/lungo_mobile.js
+++ public/js/lungo_mobile.js
@@ -1,3 +1,8 @@
+Lungo.init({
+  name: 'Activity Explorer',
+  resources: ['/partials/mobile.jade']
+});
+
 var App = (function(lng, undefined) {
 
     sectionTrigger = function(event) {
@@ -32,42 +37,3 @@ var App = (function(lng, undefined) {
     };
 
 })(Lungo);
-
-App.carousel = {prev: null, next: null};
-
-Lungo.Events.init({
-
-});
-
-Lungo.ready(function() {
-
-    // Lungo.Aside.show();
-    // Lungo.Router.section("notification");
-
-    // Lungo.Notification.show();
-    // Lungo.Notification.show("home", "Please wait...");
-    // Lungo.Notification.show("magic");
-
-    // Lungo.Notification.show("Please wait", "user", 2, function(){ alert(1); });
-
-    // Lungo.Notification.error('Lorem ipsum dolor sit amet', "    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis veritatis similique sed qui doloribus inventore doloremque temporibus ab totam...", 'remove');
-    // Lungo.Notification.success('Lorem ipsum dolor sit amet', "    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis veritatis similique sed qui doloribus inventore doloremque temporibus ab totam...", 'ok');
-    // Lungo.Notification.confirm({
-    //     icon: 'user',
-    //     title: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
-    //     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo amet nulla dolorum hic eum debitis dolorem expedita? Commodi molestiae tempora totam explicabo sed deserunt cum iusto eos perspiciatis ea in.',
-    //     accept: {
-    //         icon: 'checkmark',
-    //         label: 'Accept',
-    //         callback: function(){ alert("Yes!"); }
-    //     },
-    //     cancel: {
-    //         icon: 'close',
-    //         label: 'Cancel',
-    //         callback: function(){ alert("No!"); }
-    //     }
-    // });
-    // Lungo.Notification.html("<h1 class='title'>Title</h1><article>aslkdkals</article><a href='#' class='button large anchor' >Seleccionar</a>", "Cancelar");
-    // Lungo.Notification.push("Lorem ipsum dolor sit amet", "home");
-
-});
