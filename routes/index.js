239c740313d95f285993c831529fde576e74f11d
Rick Kleinhans <rick@baobablogic.com>
2013-04-18 21:39:42 +0200

    Update User Agent

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,9 @@ exports.index = function(req, res){
   if(/mobile/i.test(ua)) {
     res.render('mobile_index', { title: 'Activity Explorer - Experience More', layout: 'mobile_layout' });
   } 
+  else if (/android/i.test(ua)) {
+    res.render('desktop_index', { title: 'Activity Explorer - Experience More', layout: 'desktop_layout' });
+  }
   else {
     res.render('desktop_index', { title: 'Activity Explorer - Experience More', layout: 'desktop_layout' });
   }
